56ad8c1f4bebcc28a6c45da9e8359b3ea00013b6
Amankumar Shrivastava <shrivastavaman171@gmail.com>
2023-11-01 22:39:39 +0530

    Setting up header & players

---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const Player = ({ initialName, symbol }) => {
+
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((prevState) => !prevState);
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = <input type='text' value={playerName} onChange={handleChange} required />;
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+        </li>
+    )
+}
+
+export default Player
